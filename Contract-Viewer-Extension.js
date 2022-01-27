dbc2faa130b64328c6fa66276f0323e430217af2
EndureBlaze <hi@imyan.ren>
2022-01-27 18:12:21 +0800

    :sparkles: init

---
+++ Contract-Viewer-Extension.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Contract Viewer Extension
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Extension for VS Code Extension Contract Viewer
+// @author       EndureBlaze
+// @match        https://etherscan.io/address/*
+// @match        https://bscscan.com/address/*
+// @icon         https://www.google.com/s2/favicons?domain=segmentfault.com
+// @run-at document-end
+// @updateURL    
+// @grant       GM_addStyle
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // 插入下载按钮
+  GM_addStyle(".download-btn:hover { color: #3498db; }");
+  let downloadBtn = document.createElement("div");
+  downloadBtn.innerHTML =
+    '<a class="download-btn" href="javascript:;" style="color: #000">Download Contract   </a>';
+  downloadBtn.addEventListener("click", downloadContract);
+  let navBar = document.querySelector(
+    "div.flex-wrap:nth-child(1) > div:nth-child(2)"
+  );
+  navBar.insertBefore(downloadBtn, navBar.firstChild);
+
+  function downloadContract() {
+    if (document.domain === "etherscan.io") {
+      console.log(parserLink("eth"));
+      window.open(parserLink("eth"));
+    }
+    if (document.domain === "bscscan.com") {
+      window.open(parserLink("bsc"));
+    }
+  }
+
+  function parserLink(type) {
+    let url = document.location.toString();
+    let addr = url.substring(url.lastIndexOf("/"), url.length);
+    return `vscode://Metaplasia.contract-viewer/download?type=${type}&addr=${addr}`;
+  }
+})();
---
+++ README.md
@@ -0,0 +1,13 @@
+# Contract Viewer Extension
+
+[点击安装](Contract-Viewer-Extension.js)
+
+本油猴脚本是针对 VS Code 扩展 [Contract Viewer](https://github.com/MetaplasiaTeam/vscode-contract-viewer) 的一个简单的插件。
+
+会在打开合约网页的时候在页面插入一个下载按钮，点击即可在 VS Code 中下载对应的合约，尚在测试中，如果有问题请提 issue。
+
+## 更新日志
+
+### 0.1
+
+- 初始版本，支持以太坊合约和币安合约
